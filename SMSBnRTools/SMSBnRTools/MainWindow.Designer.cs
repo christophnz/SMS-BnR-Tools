6114a127a29ac7b58c4bd4023e0f3aa5cbde6cc5
christophnz <slimchrissy@gmail.com>
2014-11-05 00:17:41 +1300

    First version
    Commit of first version, developed 9-10 Aug 2014

---
+++ SMSBnRTools/SMSBnRTools/MainWindow.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace SMSBnRTools
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filePathInput = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.contactsGV = new System.Windows.Forms.DataGridView();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contactnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contactBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.messagesGV = new System.Windows.Forms.DataGridView();
+            this.readabledateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bodyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.smsesSmsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnExportSelected = new System.Windows.Forms.Button();
+            this.btnExportAll = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.contactsGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contactBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.messagesGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smsesSmsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.resultLabel);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.filePathInput);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(869, 72);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SMS Backup && Restore File";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(7, 47);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filePathInput
+            // 
+            this.filePathInput.Location = new System.Drawing.Point(7, 20);
+            this.filePathInput.Name = "filePathInput";
+            this.filePathInput.Size = new System.Drawing.Size(427, 20);
+            this.filePathInput.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "XML files|*.xml";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 90);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnMerge);
+            this.splitContainer1.Panel1.Controls.Add(this.btnDelete);
+            this.splitContainer1.Panel1.Controls.Add(this.contactsGV);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.messagesGV);
+            this.splitContainer1.Panel2.Controls.Add(this.btnExportSelected);
+            this.splitContainer1.Panel2.Controls.Add(this.btnExportAll);
+            this.splitContainer1.Size = new System.Drawing.Size(869, 493);
+            this.splitContainer1.SplitterDistance = 260;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMerge.Enabled = false;
+            this.btnMerge.Location = new System.Drawing.Point(132, 4);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(125, 23);
+            this.btnMerge.TabIndex = 2;
+            this.btnMerge.Text = "Merge Selected";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(4, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(125, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete Selected";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // contactsGV
+            // 
+            this.contactsGV.AllowUserToAddRows = false;
+            this.contactsGV.AllowUserToDeleteRows = false;
+            this.contactsGV.AllowUserToResizeRows = false;
+            this.contactsGV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.contactsGV.AutoGenerateColumns = false;
+            this.contactsGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.contactsGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.contactsGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.addressDataGridViewTextBoxColumn,
+            this.contactnameDataGridViewTextBoxColumn});
+            this.contactsGV.DataSource = this.contactBindingSource;
+            this.contactsGV.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.contactsGV.Location = new System.Drawing.Point(3, 33);
+            this.contactsGV.Name = "contactsGV";
+            this.contactsGV.RowHeadersVisible = false;
+            this.contactsGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.contactsGV.Size = new System.Drawing.Size(254, 454);
+            this.contactsGV.TabIndex = 0;
+            this.contactsGV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.contactsGV_CellClick);
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Number";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.ReadOnly = true;
+            this.addressDataGridViewTextBoxColumn.Width = 69;
+            // 
+            // contactnameDataGridViewTextBoxColumn
+            // 
+            this.contactnameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.contactnameDataGridViewTextBoxColumn.DataPropertyName = "contact_name";
+            this.contactnameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.contactnameDataGridViewTextBoxColumn.Name = "contactnameDataGridViewTextBoxColumn";
+            this.contactnameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // contactBindingSource
+            // 
+            this.contactBindingSource.DataSource = typeof(SMSBnRTools.classes.contact);
+            // 
+            // messagesGV
+            // 
+            this.messagesGV.AllowUserToResizeRows = false;
+            this.messagesGV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messagesGV.AutoGenerateColumns = false;
+            this.messagesGV.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.messagesGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.messagesGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.readabledateDataGridViewTextBoxColumn,
+            this.typeDataGridViewTextBoxColumn,
+            this.bodyDataGridViewTextBoxColumn});
+            this.messagesGV.DataSource = this.smsesSmsBindingSource;
+            this.messagesGV.Location = new System.Drawing.Point(4, 4);
+            this.messagesGV.Name = "messagesGV";
+            this.messagesGV.RowHeadersVisible = false;
+            this.messagesGV.Size = new System.Drawing.Size(598, 453);
+            this.messagesGV.TabIndex = 2;
+            this.messagesGV.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.messagesGV_CellFormatting);
+            // 
+            // readabledateDataGridViewTextBoxColumn
+            // 
+            this.readabledateDataGridViewTextBoxColumn.DataPropertyName = "readable_date";
+            this.readabledateDataGridViewTextBoxColumn.HeaderText = "Date";
+            this.readabledateDataGridViewTextBoxColumn.Name = "readabledateDataGridViewTextBoxColumn";
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "type";
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            // 
+            // bodyDataGridViewTextBoxColumn
+            // 
+            this.bodyDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.bodyDataGridViewTextBoxColumn.DataPropertyName = "body";
+            this.bodyDataGridViewTextBoxColumn.HeaderText = "Message";
+            this.bodyDataGridViewTextBoxColumn.Name = "bodyDataGridViewTextBoxColumn";
+            // 
+            // smsesSmsBindingSource
+            // 
+            this.smsesSmsBindingSource.DataSource = typeof(smsesSms);
+            // 
+            // btnExportSelected
+            // 
+            this.btnExportSelected.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExportSelected.Location = new System.Drawing.Point(85, 466);
+            this.btnExportSelected.Name = "btnExportSelected";
+            this.btnExportSelected.Size = new System.Drawing.Size(223, 23);
+            this.btnExportSelected.TabIndex = 1;
+            this.btnExportSelected.Text = "Export messages for selected contact";
+            this.btnExportSelected.UseVisualStyleBackColor = true;
+            this.btnExportSelected.Click += new System.EventHandler(this.btnExportSelected_Click);
+            // 
+            // btnExportAll
+            // 
+            this.btnExportAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExportAll.Location = new System.Drawing.Point(4, 466);
+            this.btnExportAll.Name = "btnExportAll";
+            this.btnExportAll.Size = new System.Drawing.Size(75, 23);
+            this.btnExportAll.TabIndex = 0;
+            this.btnExportAll.Text = "Export All";
+            this.btnExportAll.UseVisualStyleBackColor = true;
+            this.btnExportAll.Click += new System.EventHandler(this.btnExportAll_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "xml";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 595);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainWindow";
+            this.Text = "SMS Backup & Restore Tools for Windows";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.contactsGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contactBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.messagesGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smsesSmsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filePathInput;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView contactsGV;
+        private System.Windows.Forms.DataGridView messagesGV;
+        private System.Windows.Forms.Button btnExportSelected;
+        private System.Windows.Forms.Button btnExportAll;
+        private System.Windows.Forms.BindingSource smsesSmsBindingSource;
+        private System.Windows.Forms.BindingSource contactBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn readabledateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bodyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contactnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ SMSBnRTools/SMSBnRTools/classes/contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSBnRTools.classes
+{
+    class contact
+    {
+        public const string UNKNOWN_NAME = "(Unknown)";
+        public string address { get; set; }
+        public string contact_name { get; set; }
+        public List<smsesSms> smses { get; set; }
+    }
+}
